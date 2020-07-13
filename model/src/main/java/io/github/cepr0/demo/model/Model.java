f2698b5a6b5e26da00068a465c0365de3fa820f0
Thien Phuc <tranthienphuc0102@gmail.com>
2020-07-13 17:16:46 +0700

    commit

--- model/src/main/java/io/github/cepr0/demo/model/Model.java
+++ model/src/main/java/io/github/cepr0/demo/model/Model.java
@@ -1,21 +1,27 @@
 package io.github.cepr0.demo.model;
 
-import io.github.cepr0.demo.support.Cuid;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.github.cepr0.demo.support.Cuid;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
 import java.time.Instant;
 
 import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "models")
 public class Model {
-	
+
 	@Id
 	@Column(columnDefinition = "text")
 	private String id;
@@ -36,4 +42,5 @@ public class Model {
 		id = Cuid.createCuid();
 		createdAt = Instant.now();
 	}
+
 }
---
+++ service/src/main/resources/CreateTableScript.sql
@@ -0,0 +1,5 @@
+CREATE TABLE models (
+	id varchar(255),
+	created_at timestamp,
+	tenant varchar(255)
+);
